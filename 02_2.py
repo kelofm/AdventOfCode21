8123872f07f3730a90e8c1aead45469c602d4a36
kelofm <95422876+kelofm@users.noreply.github.com>
2021-12-03 09:46:58 +0100

    Update 02_2.py

--- 02_2.py
+++ 02_2.py
@@ -1,1 +1,18 @@
-print((lambda c: c.real*c.imag)((lambda l: sum(v.real*v.imag+v.imag*1j for v in l))((lambda l: [sum(v[1] for v in l[:I] if v[0]=='d')-sum(v[1] for v in l[:I] if v[0]=='u')+V[1]*1j for I,V in enumerate(l) if V[0]=='f']) ([[l[0],int(l.strip()[l.rfind(' ')+1:])] for l in open("input").readlines()]))))
+# Parse input to command char - value pairs
+commands = [[l[0],int(l.strip()[l.rfind(' ')+1:])] for l in open("input").readlines()]
+
+# Compute aim at "forward" commands
+# and store it along with the value of the command
+forwardCommands = [[sum(v[1] for v in commands[:I] if v[0]=='d')-sum(v[1] for v in commands[:I] if v[0]=='u'),V[1]] for I,V in enumerate(commands) if V[0]=='f']
+
+# Execute "forward" commands
+# and represent the position on the complex plane
+#   real part: depth
+#   imag part: horizontal distance
+position = sum(pair[0]*pair[1] + pair[1]*1j for pair in forwardCommands)
+
+# Result
+print(position.real * position.imag)
+
+# One-liner
+print((lambda c: c.real*c.imag)((lambda l: sum(v[0]*v[1]+v[1]*1j for v in l))((lambda l: [[sum(v[1] for v in l[:I] if v[0]=='d')-sum(v[1] for v in l[:I] if v[0]=='u'),V[1]] for I,V in enumerate(l) if V[0]=='f'])([[l[0],int(l.strip()[l.rfind(' ')+1:])] for l in open("input").readlines()]))))
