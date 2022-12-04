dde01167037a46bded2d43b66986a76c2d9f2883
matekelemen <kelemen.mate.12@gmail.com>
2022-12-04 11:13:34 +0100

    add day 4 rust

---
+++ 22/day_4/part_2/src/main.rs
@@ -0,0 +1,48 @@
+#![allow(non_snake_case)]
+
+// --- STD Imports ---
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+
+
+fn readLines<P>(fileName: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where P: AsRef<Path>
+{
+    Ok(io::BufReader::new(std::fs::File::open(fileName)?).lines())
+}
+
+
+fn isOverlapping(l: &Vec<i64>, r: &Vec<i64>) -> bool
+{
+    return ((l[0] < r[0]) != (l[0] <= r[1])) || ((l[1] < r[0]) != (l[1] <= r[1]));
+}
+
+
+fn predicate<'a, T>(mut it: T) -> bool
+where T: Iterator<Item = Vec<i64>>
+{
+    let l = it.next().unwrap();
+    let r = it.next().unwrap();
+    return isOverlapping(&l, &r) || isOverlapping(&r, &l);
+}
+
+
+fn main()
+{
+    let mut sum: i64 = 0;
+
+    if let Ok(lines) = readLines("input") {
+        for line in lines {
+            if let Ok(string) = line {
+                sum += predicate(
+                    string.split(',')
+                          .map(|s| s.split('-').map(|b| b.parse::<i64>().unwrap())
+                          .collect::<Vec<i64>>())
+                ) as i64;
+            } // if string
+        } // for line in lines
+    } // if lines
+
+    println!("{}", sum);
+}
